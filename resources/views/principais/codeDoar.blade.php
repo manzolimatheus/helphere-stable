c29a3d1141f5331d6b3f00381e8d0fcd7bf6b5cc
GrupoHelpHere <helphere.contato@gmail.com>
2021-10-08 19:26:28 -0300

    Update codeDoar.blade.php

--- resources/views/principais/codeDoar.blade.php
+++ resources/views/principais/codeDoar.blade.php
@@ -16,6 +16,12 @@ use \App\Pix\Payload;
 use Mpdf\QrCode\QrCode;
 use Mpdf\QrCode\Output;
 
+//bacon qrcode
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\Image\SvgImageBackEnd;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+
 
 
 // GERA O VALOR DO TXID
@@ -49,58 +55,68 @@ $obPayload = (new Payload)->setPixKey($institute->pixKey)
 $payloadQrCode = $obPayload->getPayload();
 
 //QR CODE
-$obQrCode = new QrCode($payloadQrCode);
+//$obQrCode = new QrCode($payloadQrCode);
 
 // IMAGEM DO QRCODE
-$image = (new Output\Png)->output($obQrCode, 400);
+//$image = (new Output\Png)->output($obQrCode, 400);
+
+$renderer = new ImageRenderer(
+    new RendererStyle(400),
+    new SvgImageBackEnd()
+);
+$writer = new Writer($renderer);
+
 
 
 ?>
 
-    <div class="container bg-white rounded shadow p-3 mt-3 w-100 text-center" style="height: fit-content">
+<div class="container bg-white rounded shadow p-3 mt-3 w-100 text-center" style="height: fit-content">
 
     <div class="col">
-                        <div class="container bg-white rounded shadow p-3 mt-3 w-100 text-center" style="height: fit-content">
-                        <br>
-                            <b>
-                            <h1 class="text-center"><strong><i>QR CODE PIX PARA A DOAÇÃO</i></strong></h1>
-                            </b>
-                            <br>
-                        </div>
-                    </div>
-        
-            <br />
-            <br />
-
+        <div class="container bg-white rounded shadow p-3 mt-3 w-100 text-center" style="height: fit-content">
             <br>
-            <div class=" col text-center">
-
-                <img src="data:image/png;base64, <?= base64_encode($image) ?> " class="img-fluid">
-            </div>
-            <br><br>
-
-            <h4><strong>Código do Pix:</strong></h4><br>
-
-            <div class="row">
-                <div class="col-2">
-                    <button class="btn bg-light border" onclick="copy()">
-                        <ion-icon name="clipboard-outline" class="h2"></ion-icon>
-                    </button>
-                </div>
-                <div class="col-10">
-                    <div class="container">
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="link">
-                                <ion-icon name="link-outline" class="h3"></ion-icon>
-                            </span>
-                            <input type="text" class="form-control text-muted bg-white" readonly value="<?= $payloadQrCode ?>" aria-describedby="link" id="linkToCopy">
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
+            <b>
+                <h1 class="text-center"><strong><i>QR CODE PIX PARA A DOAÇÃO</i></strong></h1>
+            </b>
+            <br>
+        </div>
     </div>
+
+    <br />
+    <br />
+
+    <br>
+    <div class=" col text-center img-fluid">
+
+        <?php
+        //Exibe o QR code na tela
+        echo ($writer->writeString($payloadQrCode));
+        ?>
+    </div>
+    <br><br>
+
+    <h4><strong>Código do Pix:</strong></h4><br>
+
+    <div class="row">
+        <div class="col-2">
+            <button class="btn bg-light border" onclick="copy()">
+                <ion-icon name="clipboard-outline" class="h2"></ion-icon>
+            </button>
+        </div>
+        <div class="col-10">
+            <div class="container">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="link">
+                        <ion-icon name="link-outline" class="h3"></ion-icon>
+                    </span>
+                    <input type="text" class="form-control text-muted bg-white" readonly value="<?= $payloadQrCode ?>" aria-describedby="link" id="linkToCopy">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
 
 <div class="text-center">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg/2560px-Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg.png" alt="Pix" class="w-50 p-3 img-fluid">
