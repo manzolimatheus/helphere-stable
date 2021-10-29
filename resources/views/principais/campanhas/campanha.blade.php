a138e63256a7afac86572961edccbdd30ac890d3
SCOUTBR <85500921+SCOUTBR@users.noreply.github.com>
2021-10-29 20:13:41 -0300

    Update campanha.blade.php

--- resources/views/principais/campanhas/campanha.blade.php
+++ resources/views/principais/campanhas/campanha.blade.php
@@ -238,14 +238,11 @@
         function copy() {
             /* Get the text field */
             var copyText = document.getElementById("linkToCopy");
-
             /* Select the text field */
             copyText.select();
             copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
             /* Copy the text inside the text field */
             document.execCommand("copy");
-
             /* Alert the copied text */
             alert("Texto copiado para a área de transferência!");
         }
